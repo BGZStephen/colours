e27359dc7cde69aefb3889d3102aedbc09df9b9f
Stephen Wright <sjw948@gmail.com>
2017-05-15 22:28:36 +0100

    base node / express config + ng setup
    express, cors,

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser')
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./config/database')
+
+// conncet to database
+
+mongoose.connect(config.database)
+
+// on success
+mongoose.connection.on('connected', () => {
+  console.log("Connected to database: " + config.database)
+})
+
+// on error
+mongoose.connection.on('error', (err) => {
+  console.log("Error: " + err)
+})
+
+// set servr
+const app = express()
+
+// middleware
+
+// cors setup
+app.use(cors())
+
+// static folder for public views
+app.use(express.static(path.join(__dirname, 'public')))
+
+// body partser initialize
+app.use(bodyParser.json())
+
+// routing
+
+// make routes accessible
+// const users = require("./routes/users")
+// app.use('/users', users)
+
+// index route
+app.get('/', (req, res) => {
+  res.send('Invalid Endpoint')
+})
+
+// default route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+// start server
+
+// define port
+const port = 3006
+
+app.listen(port, () => {
+  console.log("Server started, listening on port " + port)
+})
