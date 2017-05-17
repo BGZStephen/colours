df139848286d29a838daa0bd7b70903c64effa3a
Stephen Wright <sjw948@gmail.com>
2017-05-17 11:20:17 +0100

    finished responsive home-navbar component

--- angular-src/src/app/components/home-navbar/home-navbar.component.ts
+++ angular-src/src/app/components/home-navbar/home-navbar.component.ts
@@ -7,7 +7,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeNavbarComponent implements OnInit {
 
-  constructor() { }
+  colours: Array<object> = [
+    {"background": "#00be9c", "color": "#1D1F25"},
+    {"background": "#20ce6d", "color": "#1D1F25"},
+    {"background": "#2c97df", "color": "#f5f5f5"},
+    {"background": "#9c56b8", "color": "#f5f5f5"},
+    {"background": "#ecf0f1", "color": "#1D1F25"},
+    {"background": "#f3c500", "color": "#1D1F25"},
+    {"background": "#e87e04", "color": "#f5f5f5"},
+    {"background": "#ea4b36", "color": "#f5f5f5"}
+  ]
+
+  activeHover: number = -1 // used to check with LI is being hovered, if none, revert to -1
+
+  setHoverStyle(index) { // triggered on both mouse over and mouse out events to toggle application of applyHoverStyle
+    if(index == this.activeHover) {
+      this.activeHover = -1
+    } else {
+      this.activeHover = index
+    }
+  }
+
+  applyHoverStyle(index) {
+    if(index == this.activeHover) {
+      let max = Math.floor(this.colours.length);
+      let randomStyle = Math.floor(Math.random() * max)
+      return this.colours[randomStyle]
+    }
+  }
+
+  navbarVisibility: boolean = false;
+
+  resizeToggleNavbar() {
+    if(screen.width > 1024) {
+      this.navbarVisibility = true;
+    }
+  }
+
+  setNavbarVisibility() {
+    if(this.navbarVisibility == true) {
+      return {"left": "0"}
+    } else {
+      return {"left": "-100%"}
+    }
+  }
+
+  setActionBarVisibility() {
+    if(this.navbarVisibility == true) {
+      return {"right": "0%"}
+    } else {
+      return {"right": "-100%"}
+    }
+  }
+
+  constructor() {
+    this.resizeToggleNavbar()
+  }
+
 
   ngOnInit() {
   }
