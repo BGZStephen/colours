c7a6445bd971ed04064eb9ccd2bd9d8617bcf788
Stephen Wright <sjw948@gmail.com>
2017-07-01 20:24:43 +0100

    refactored update profile / password api endpoint to a single put call

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,6 @@ const config = require('../config');
 const User = require('../models/user');
 const ColourLibrary = require('../models/colour-library');
 
-// admin update password
-
 // authanticate
 router.post("/authenticate", (req, res, next) => {
 
@@ -160,64 +158,67 @@ router.post("", (req, res, next) => {
   }
 })
 
-// update user
-router.put("", (req, res, next) => {
-
-  let userObject = {
-    email: req.body.email,
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    username: req.body.username,
-    _id: req.body._id
-  }
-
-  // check username to make sure it doesn't already exist except in the case of it being the updaters current username
-  User.getOne({username: userObject.username})
-  .then(result => {
-    if(result._id != userObject._id) {
-      return Promise.reject({error: "Username already taken"})
-    } else {
-      return User.getOne({email: userObject.email})
-    }
-  }).then(result => {
-    if(result != null && result.email != userObject.email) {
-      return Promise.reject({error: "Email address already registered"})
-    } else {
-      return User.updateUser(userObject)
-    }
-  }).then(result => {
-    res.sendStatus(200)
-  }).catch(error => {
-    console.log(error)
-    res.statusMessage = error.error
-    res.status(500).send(error)
-  })
-})
-
 // update password
-router.post("/updatepassword", (req, res, next) => {
+router.put("/:userId", (req, res, next) => {
 
-  let userObject = {
-    _id: req.body._id,
-    queryPassword: req.body.currentPassword,
-    newPassword: req.body.newPassword
+  // used to determine if it's a profile update or password update
+  let updateType = req.body.type
+
+  if(updateType == "profile") {
+    let userObject = {
+      email: req.body.email,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      username: req.body.username,
+      _id: req.body._id
+    }
+
+    // check username to make sure it doesn't already exist except in the case of it being the updaters current username
+    User.getOne({username: userObject.username})
+    .then(result => {
+      if(result._id != userObject._id) {
+        return Promise.reject({error: "Username already taken"})
+      } else {
+        return User.getOne({email: userObject.email})
+      }
+    }).then(result => {
+      if(result != null && result.email != userObject.email) {
+        return Promise.reject({error: "Email address already registered"})
+      } else {
+        return User.updateUser(userObject)
+      }
+    }).then(result => {
+      res.sendStatus(200)
+    }).catch(error => {
+      console.log(error)
+      res.statusMessage = error.error
+      res.status(500).send(error)
+    })
+
+    // if not a profile update, check if it's a password update
+  } else if (updateType == "password") {
+    let userObject = {
+      _id: req.body._id,
+      queryPassword: req.body.currentPassword,
+      newPassword: req.body.newPassword
+    }
+
+    User.getOne({_id: userObject._id})
+    .then(result => {
+      userObject.storedHash = result.password
+      return User.comparePassword(userObject)
+    })
+    .then(() => {
+      return User.updatePassword(userObject)
+    })
+    .then(result => {
+      res.sendStatus(200)
+    }).catch(error => {
+      console.log(error)
+      res.statusMessage = error
+      res.status(500).send(error)
+    })
   }
-
-  User.getOne({_id: userObject._id})
-  .then(result => {
-    userObject.storedHash = result.password
-    return User.comparePassword(userObject)
-  })
-  .then(() => {
-    return User.updatePassword(userObject)
-  })
-  .then(result => {
-    res.sendStatus(200)
-  }).catch(error => {
-    console.log(error)
-    res.statusMessage = error
-    res.status(500).send(error)
-  })
 })
 
 module.exports = router;
