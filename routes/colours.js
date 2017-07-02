b12a95f8b736bdb4e6d48a4cd1e526314addd737
Stephen Wright <sjw948@gmail.com>
2017-07-02 20:45:23 +0100

    added jwt authentication to colours API endpoints

--- routes/colours.js
+++ routes/colours.js
@@ -9,100 +9,122 @@ const ColourLibrary = require('../models/colour-library')
 // create a Colour and add it to the users colour library
 router.post("", (req, res, next) => {
 
-  if(req.body.type == "library") {
-    let colourObject = new Colour({
-      createdAt: new Date(),
-      createdBy: req.body.createdBy,
-      hex: req.body.hex
-    })
+  if(!req.get('Authorization')) {
+    return res.status(401).json({error: "Authorisation token not supplied"})
+  }
 
+  let verifiedJwt = jwt.verify(req.get('Authorization'), config.secret)
 
-    Colour.create(colourObject)
-    .then(result => {
-      result.colour.createdBy = req.body.createdBy;
-      // pushes the colour Object to the Colours array within the Colour Library
-      return ColourLibrary.addColourToLibrary(result.colour)
-    }).then(result => {
-      res.json(result)
-    })
-    .catch(error => {
-      res.json(error)
-    })
-  } else if(req.body.type == "palette") {
-    let colourObject = new Colour({
-      createdAt: new Date(),
-      createdBy: req.body.createdBy,
-      hex: req.body.hex
-    })
+  if(verifiedJwt == undefined) {
+    res.status(403).json({error: "Authorization token not valid"})
+  } else {
 
-    let paletteObject = {
-      paletteId: req.body.paletteId,
-    }
-
-    Colour.create(colourObject)
-    .then(result => {
-      // define the new PaletteItem which fits the corresponding schema
-      let paletteItemObject = new PaletteItem({
+    if(req.body.type == "library") {
+      let colourObject = new Colour({
         createdAt: new Date(),
         createdBy: req.body.createdBy,
-        colour: {
-          createdAt: result.colour.createdAt,
-          createdBy: result.colour.createdBy,
-          hex: result.colour.hex
-        },
-        description: req.body.description
+        hex: req.body.hex
       })
-      //Create a colour and then add it to the newly created paletteItem
-      return PaletteItem.createPaletteItem(paletteItemObject)
-    }).then(result => {
-      /*
-      final backup function in the event that the colour is not already in the
-      users library, this will ensure it's pushed there also
-      */
 
-      /*
-      using the paletteId outlined in the paetteObject,
-      push the newly created paletteItem to it
-      */
-      return Palette.addPaletteItem(result, paletteObject)
-    }).then(result => {
-      res.json(result)
-    })
-    .catch(error => {
-      res.json(error)
-    })
-    ColourLibrary.addColourToLibrary(colourObject)
+
+      Colour.create(colourObject)
+      .then(result => {
+        result.colour.createdBy = req.body.createdBy;
+        // pushes the colour Object to the Colours array within the Colour Library
+        return ColourLibrary.addColourToLibrary(result.colour)
+      }).then(result => {
+        res.json(result)
+      })
+      .catch(error => {
+        res.json(error)
+      })
+    } else if(req.body.type == "palette") {
+      let colourObject = new Colour({
+        createdAt: new Date(),
+        createdBy: req.body.createdBy,
+        hex: req.body.hex
+      })
+
+      let paletteObject = {
+        paletteId: req.body.paletteId,
+      }
+
+      Colour.create(colourObject)
+      .then(result => {
+        // define the new PaletteItem which fits the corresponding schema
+        let paletteItemObject = new PaletteItem({
+          createdAt: new Date(),
+          createdBy: req.body.createdBy,
+          colour: {
+            createdAt: result.colour.createdAt,
+            createdBy: result.colour.createdBy,
+            hex: result.colour.hex
+          },
+          description: req.body.description
+        })
+        //Create a colour and then add it to the newly created paletteItem
+        return PaletteItem.createPaletteItem(paletteItemObject)
+      }).then(result => {
+        /*
+        final backup function in the event that the colour is not already in the
+        users library, this will ensure it's pushed there also
+        */
+
+        /*
+        using the paletteId outlined in the paetteObject,
+        push the newly created paletteItem to it
+        */
+        return Palette.addPaletteItem(result, paletteObject)
+      }).then(result => {
+        res.json(result)
+      })
+      .catch(error => {
+        res.json(error)
+      })
+      ColourLibrary.addColourToLibrary(colourObject)
+    }
   }
 })
 
 // delete colour from Library
 router.put("/colourId", (req, res, next) => {
 
-  if(req.body.type == "library") {
-    let colourObject = {
-      createdBy: req.body.createdBy,
-      colourId: req.body.colourId
-    }
+  if(!req.get('Authorization')) {
+    return res.status(401).json({error: "Authorisation token not supplied"})
+  }
 
-    ColourLibrary.deleteColourFromLibrary(colourObject)
-    .then(result => {
-      res.json(result)
-    }).catch(error => {
-      res.json(error)
-    })
-  } else if (req.body.type == "palette") {
-    let paletteItemObject = {
-      paletteId: req.body.paletteId,
-      paletteItemId: req.body.paletteItemId
-    }
+  let verifiedJwt = jwt.verify(req.get('Authorization'), config.secret)
 
-    Palette.deletePaletteItem(paletteItemObject)
-    .then(PaletteItem.deletePaletteItem({_id: paletteItemObject.paletteItemId}))
-    .then(result => {
-      res.json(result)
-    }).catch(error => {
-      res.json(error)
-    })
+  if(verifiedJwt == undefined) {
+    res.status(403).json({error: "Authorization token not valid"})
+  } else {
+
+    if(req.body.type == "library") {
+      let colourObject = {
+        createdBy: req.body.createdBy,
+        colourId: req.body.colourId
+      }
+
+      ColourLibrary.deleteColourFromLibrary(colourObject)
+      .then(result => {
+        res.json(result)
+      }).catch(error => {
+        res.json(error)
+      })
+    } else if (req.body.type == "palette") {
+      let paletteItemObject = {
+        paletteId: req.body.paletteId,
+        paletteItemId: req.body.paletteItemId
+      }
+
+      Palette.deletePaletteItem(paletteItemObject)
+      .then(PaletteItem.deletePaletteItem({_id: paletteItemObject.paletteItemId}))
+      .then(result => {
+        res.json(result)
+      }).catch(error => {
+        res.json(error)
+      })
+    }
   }
 })
 
